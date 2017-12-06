1150ed408511235558aaee04ec80a4b0794b744d
rodamber <rodrigoambernardo@gmail.com>
2017-12-06 04:53:02 +0100

    Implemented set union and ordering.

--- Automaton.scala
+++ Automaton.scala
@@ -19,134 +19,153 @@ object Utils {
     }
   }
 
-}
 
-import Utils._
-
-
-
-
-case class Automaton[State](
-  E: List[Char],                   // alphabet
-  S: List[State],                  // internal states
-  M: (State, Char) => List[State], // transition function
-  S0: List[State],                 // initial states
-  F: List[State]                   // final states
-) {
-  require(E.nonEmpty)
-  require(S.nonEmpty)
-
-  require(forall((s: State, c: Char) =>
-            M(s,c).content subsetOf S.content))
-
-
-  // require(forall((s: State, c: Char) =>
-  //           !(S.contains(s) && E.contains(c)) ==> (M(s,c) == Nil[State]())))
-
-  require(S0.content subsetOf S.content)
-  require(F.content subsetOf S.content)
-
-
-  def validSet(L: List[State]): Boolean = {
-    validSetAux(S, L)
-  }
-
-  def validSetAux(S: List[State], L: List[State]): Boolean = {
-    (S, L) match {
-      case (_, Nil()) => true
-      case (Nil(), _) => false
-      case (Cons(s, ss), Cons(l, ls)) => {
-        if (s == l) validSetAux(ss, ls)
-        else validSetAux(ss, L)
+  def isSubsequenceOf[T](L: List[T], S: List[T]): Boolean = {
+    (L, S) match {
+      case (Nil(), _) => true
+      case (_, Nil()) => false
+      case (Cons(l, ls), Cons(s, ss)) => {
+        if (l == s) isSubsequenceOf(ls, ss)
+        else isSubsequenceOf(L, ss)
       }
     }
   }
 
 
-  def isDeterministic: Boolean =
-    S0.size == 1 && forall { (s: State, c: Char) =>
-      ((S contains s) && (E contains c)) ==> (M(s, c).size <= 1)
-    }
-
-
-  def isAcceptingPath(p: List[State], x: List[Char]): Boolean = {
-    require(p.content subsetOf S.content)
-    require(x.content subsetOf E.content)
-
-    p match {
-      case Nil() => false
-      case Cons(sn, _) => F.contains(sn) && isPath(p, x, S0)
-    }
-  } ensuring { res =>
-    res ==> (p.size == x.size + 1)
+  // Same as isSorted, but with a custom order.
+  def isOrdered[T](L: List[T], leq: (T, T) => Boolean): Boolean = L match {
+    case Nil()          => true
+    case Cons(_, Nil()) => true
+    case Cons(a, tail @ Cons(b, _)) => leq(a, b) && isOrdered(tail, leq)
   }
 
 
-  def isPath(p: List[State], x: List[Char], from: List[State]): Boolean = {
-    require(p.content subsetOf S.content)
-    require(x.content subsetOf E.content)
-    require(from.content subsetOf S.content)
+  def subsequenceIsOrdered[T](l: List[T], s: List[T], leq: (T, T) => Boolean): Boolean = {
+    require(isOrdered(s, leq))
+    isSubsequenceOf(l, s) ==> isOrdered(l, leq)
+  }.holds
 
-    (p, x) match {
-      case (Cons(s0, Nil()), Nil()) =>
-        from.contains(s0)
-      case (Cons(sk1, ss @ Cons(sk, _)), Cons(xk, xs)) =>
-        M(sk, xk).contains(sk1) && isPath(ss, xs, from)
+}
+
+import Utils._
+
+case class Automaton[State](
+  S: List[State],                  // internal states
+  M: (State, Char) => List[State], // transition function
+  S0: State,                       // initial states
+  F: List[State],                  // final states
+
+  // We need some way to order the states.
+  // FIXME: We'll probably have problems with this.
+  leq: (State, State) => Boolean
+) {
+  require(S.nonEmpty && isOrdered(S, leq))
+
+  require(forall((s: State, w: Char) => isSubsequenceOf(M(s, w), S)))
+
+  require(S contains S0)
+  require(isSubsequenceOf(F, S))
+
+
+  def validSet(L: List[State]): Boolean = {
+    // FIXME: The last condition should not be needed to be stated explicitly.
+    isSubsequenceOf(L, S) && isOrdered(L, leq)
+  }
+
+
+  // flatMap
+  // Removes repeated elements. Strictly ordered.
+  def unionMap(set: List[State], f: State => List[State]): List[State] = {
+    require(isOrdered(set, leq))
+    require(forall((s: State) => isOrdered(f(s), leq)))
+
+    set match {
+      case Nil()      => Nil()
+      case Cons(h, t) => merge(f(h), unionMap(t, f))
+    }
+  }
+
+
+  // Removes repeated elements. Strictly ordered.
+  def merge(a: List[State], b: List[State]): List[State] = {
+    require(isOrdered(a, leq))
+    require(isOrdered(b, leq))
+
+    (a, b) match {
+      case (_, Nil()) => a
+      case (Nil(), _) => b
+      case (Cons(x, xs), Cons(y, ys)) =>
+        if (x == y)         x :: merge(xs, ys) // FIXME
+        else if (leq(x, y)) x :: merge(xs, b)  // FIXME
+        else                y :: merge(a, ys)
+    }
+  }
+
+
+  def isDeterministic: Boolean =
+    forall { (s: State, c: Char) =>
+      (S contains s) ==> (M(s, c).size <= 1)
+    }
+
+
+  def isAcceptingPath(states: List[State], word: List[Char]): Boolean = {
+    require(states.content subsetOf S.content)
+
+    states match {
+      case Nil() => false
+      case Cons(s, _) => F.contains(s) && isPath(states, word, S0)
+    }
+  }
+
+
+  def isPath(states: List[State], word: List[Char], from: State): Boolean = {
+    require(states.content subsetOf S.content)
+    require(S contains from)
+
+    (states, word) match {
+      case (Cons(s, Nil()), Nil()) =>
+        from == s
+      case (Cons(t, ss @ Cons(s, _)), Cons(w, ws)) =>
+        M(s, w).contains(t) && isPath(ss, ws, from)
       case _ =>
         false
     }
-  } ensuring { res =>
-    res ==> (p.size == x.size + 1)
   }
 
 
-  def mkPath(x: List[Char], from: State): List[State] = {
-    require(x.content subsetOf E.content)
+  def mkPath(word: List[Char], from: State): Option[List[State]] = {
     require(S contains from)
-    // require(forall { (s: State, c: Char) => M(s, c).nonEmpty })
 
-    x match {
-      case Nil() =>
-        List(from)
-      case Cons(xk, xs) =>
-        mkPath(xs, from) match {
-          case p @ Cons(s, _) => {
-            // FIXME: Why can't it verify this?
-            // assert(M(s, xk).nonEmpty)
-
-            // FIXME: Why can it verify this but can't verify the postcondition?
-            // assert(M(s, xk).content subsetOf S.content)
-
-            M(s, xk) match {
-              case Cons(t, _) => t :: p
-            }
+    word match {
+      case Nil()       => Some(List(from))
+      case Cons(w, ws) => mkPath(ws, from) match {
+        case Some(path) => path match {
+          case Cons(s, _) => M(s, w) match {
+            case Cons(t, _) => Some(t :: path)
+            case Nil()      => None()
           }
         }
+        case None() => None()
+      }
     }
-  } ensuring { res =>
-    check(res.size == x.size + 1) && check(res.content subsetOf S.content)
   }
 
-  def runNFA(x: List[Char], from: List[State]): List[State] = {
-    require(x.content subsetOf E.content)
+
+  def runNFA(word: List[Char], from: List[State]): List[State] = {
     require(validSet(from))
 
-    x match {
-      case Nil() =>
-        from
-      case Cons(xk, xs) =>
-        from.flatMap( { M(_, c) }).content
-
-        for (s <- from; q <- M(s, c)) yield q
-
-
+    word match {
+      case Nil()       => from
+      // FIXME: There is an obvious problem here: the output of M is not
+      // guaranteed to be a valid set, so the precondition to unionMap is not
+      // verified.
+      case Cons(w, ws) => runNFA(ws, unionMap(from, { M(_, w) }))
     }
   }
 
 
   // // FIXME:
   // def mkPathIsSound(x: List[Char], p: List[State], from: State): Boolean = {
-  //   require(x.content subsetOf E.content)
   //   require(S contains from)
   //   require(p == mkPath(x, from))
 
@@ -171,41 +190,40 @@ case class Automaton[State](
   // }.holds
 
 
-  @ignore
-  // FIXME: adt invariant ?
-  def det(): Automaton[List[State]] = {
-    val T = powerSet(S)
-    val N = { (t: List[State], c: Char) =>
-      if (T.contains(t) && E.contains(c))
-        List(t.flatMap { M(_, c) })
-      else Nil[List[State]]()
-    }
-    val t0 = List(S0)
-    val G  = for (t <- T if (t & F).nonEmpty) yield t
+  // @ignore
+  // // FIXME: adt invariant ?
+  // def det(): Automaton[List[State]] = {
+  //   val T = powerSet(S)
+  //   val N = { (t: List[State], c: Char) =>
+  //     if (T.contains(t))
+  //       List(t.flatMap { M(_, c) })
+  //     else Nil[List[State]]()
+  //   }
+  //   val t0 = List(S0)
+  //   val G  = for (t <- T if (t & F).nonEmpty) yield t
 
-    Automaton(E, T, N, t0, G)
-  } ensuring { res =>
-    res.isDeterministic
-    //   && forall { (t: List[State], c: Char) =>
-    //   if ((res.S contains t) && (res.E contains c))
-    //     res.M(t, c).nonEmpty
-    //   else true
-    // }
-  }
+  //   Automaton(T, N, t0, G)
+  // } ensuring { res =>
+  //   res.isDeterministic
+  //   //   && forall { (t: List[State], c: Char) =>
+  //   //   if ((res.S contains t))
+  //   //     res.M(t, c).nonEmpty
+  //   //   else true
+  //   // }
+  // }
 
 
-  @ignore
-  def TA_in_TDA(x: List[Char], s: List[State], t: List[State]): Boolean = {
-    require(isAcceptingPath(s, x))
-    // require(D == det())
+  // @ignore
+  // def TA_in_TDA(x: List[Char], s: List[State], t: List[State]): Boolean = {
+  //   require(isAcceptingPath(s, x))
+  //   // require(D == det())
 
-    det().isAcceptingPath(t, x)
-  }.holds
+  //   det().isAcceptingPath(t, x)
+  // }.holds
 
 
   // def sk_in_tk(x: List[Char], s: List[State]): Boolean = {
   //   require(s.content subsetOf S.content)
-  //   require(x.content subsetOf E.content)
   //   require(isPath(s, x, S0))
 
   //   val D = det()
@@ -219,7 +237,6 @@ case class Automaton[State](
   //   // Postcondition of det
   //   assert(forall { (t: List[State], c: Char) =>
   //            require(D.S contains t)
-  //            require(D.E contains c)
 
   //            D.M(t, c).nonEmpty
   //          })
