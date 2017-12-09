a7b5f63b180b335bb277b675bb870285f611553c
rodamber <rodrigoambernardo@gmail.com>
2017-12-09 06:45:52 +0100

    Using sortedUnique.
    Was able to prove merge, move and runFrom :)

--- Automaton.scala
+++ Automaton.scala
@@ -10,18 +10,64 @@ import stainless.proof._
 object Utils {
 
   // Strictly decreasing
-  def strictDec(l: List[BigInt]): Boolean = {
+  def isSortedUnique(l: List[BigInt]): Boolean = {
     l match {
       case Nil() => true
       case Cons(x, Nil()) => true
-      case Cons(x, xs @ Cons(y, _)) => (x < y) && strictDec(xs)
+      case Cons(x, xs @ Cons(y, _)) => (x < y) && isSortedUnique(xs)
+    }
+  }
+
+  def sortedUnique(ls: List[BigInt]): List[BigInt] = {
+    ls match {
+      case Cons(h, t) => sortedInsert(sortedUnique(t), h)
+      case Nil()      => Nil[BigInt]()
+    }
+  }
+
+  def sortedInsert(ls: List[BigInt], v: BigInt): List[BigInt] = {
+    require(isSortedUnique(ls))
+
+    ls match {
+      case Nil() =>
+        Cons(v, Nil())
+      case Cons(h, t) =>
+        if      (v == h) ls
+        else if (v <  h) Cons(v, ls)
+        else             Cons(h, sortedInsert(t, v))
+    }
+  }
+
+  def merge(a: List[BigInt], b: List[BigInt]): List[BigInt] = {
+    require(isSortedUnique(a) && isSortedUnique(b))
+
+    (a, b) match {
+      case (Nil(), _) => b
+      case (_, Nil()) => a
+      case (Cons(x, xs), Cons(y, ys)) =>
+        if      (x == y) x :: merge(xs, ys)
+        else if (x <  y) x :: merge(xs, b)
+        else             y :: merge(a,  ys)
+    }
+  } ensuring {
+    res => isSortedUnique(res) && res.content == a.content ++ b.content
+  }
+
+  def sortedUniquePC(ls: List[BigInt]): Boolean = {
+    isSortedUnique(sortedUnique(ls))
+  }.holds because {
+    ls match {
+      case Nil() => trivial
+      case Cons(x, xs) => sortedUniquePC(xs) && sortedInsertPC(sortedUnique(xs), x)
     }
   }
 
   @induct
-  def strictDecSorted(l: List[BigInt]): Boolean = {
-    require(strictDec(l))
-    ListOps.isSorted(l)
+  def sortedInsertPC(ls: List[BigInt], v: BigInt): Boolean = {
+    require(isSortedUnique(ls))
+
+    val x = sortedInsert(ls, v)
+    isSortedUnique(x) && ((x.content == ls.content + v) || x.content == ls.content)
   }.holds
 
 }
@@ -30,17 +76,37 @@ import Utils._
 case class DFA(states: List[List[BigInt]], trans: (List[BigInt], Char) => List[BigInt]) {
   require(forall((s: List[BigInt], w: Char) => states contains trans(s, w)))
 
-  // FIXME
+  def validState(s: List[BigInt]): Boolean = (states contains s)
+
   def runFrom(word: List[Char], from: List[BigInt]): List[BigInt] = {
-    Nil()
+    require(validState(from))
+
+    word match {
+      case Nil() => from
+      case Cons(w, ws) => runFrom(ws, trans(from, w))
+    }
+  }
+
+  def runFromPC(word: List[Char], from: List[BigInt]): Boolean = {
+    require(validState(from))
+    validState(runFrom(word, from))
+  }.holds because {
+    word match {
+      case Nil() => trivial
+      case Cons(w, ws) =>
+        val to = trans(from, w)
+        (states contains to) && runFromPC(ws, to)
+    }
   }
 }
 
 case class NFA(states: List[BigInt], trans: (BigInt, Char) => List[BigInt]) {
-  require(forall((s: BigInt, w: Char) => trans(s, w).content subsetOf states.content))
+  require(
+    // isSortedUnique(states) &&
+    forall((s: BigInt, w: Char) => trans(s, w).content subsetOf states.content))
 
   def validStates(s: List[BigInt]): Boolean = {
-    (s.content subsetOf states.content) && strictDec(s)
+    (s.content subsetOf states.content) && isSortedUnique(s)
   }
 
   def move(set: List[BigInt], w: Char): List[BigInt] = {
@@ -52,28 +118,10 @@ case class NFA(states: List[BigInt], trans: (BigInt, Char) => List[BigInt]) {
     }
   }
 
-  def merge(a: List[BigInt], b: List[BigInt]): List[BigInt] = {
-    require(validStates(a) && validStates(b))
-
-    (a, b) match {
-      case (Nil(), _) => b
-      case (_, Nil()) => a
-      case (Cons(x, xs), Cons(y, ys)) =>
-        if (x < y) x :: merge(xs, b)
-        else       y :: merge(a,  ys)
-    }
-  }
-
-  def mergeLemma(a: List[BigInt], b: List[BigInt]): Boolean = {
-    require(validStates(a) && validStates(b))
-
-    strictDec(merge(a, b)) && {
-      (a, b) match {
-        case (Nil(), _) => trivial
-        case (_, Nil()) => trivial
-        case (Cons(x, xs), Cons(y, ys)) => mergeLemma(xs, b) || mergeLemma(a,  ys)
-      }
-    }
+  @induct
+  def movePC(set: List[BigInt], w: Char): Boolean = {
+    require(validStates(set))
+    validStates(move(set, w))
   }.holds
 
   // Returns the set of possible states the NFA might be in after processing the
@@ -84,6 +132,16 @@ case class NFA(states: List[BigInt], trans: (BigInt, Char) => List[BigInt]) {
     word match {
       case Nil()       => from
       case Cons(w, ws) => runFrom(ws, move(from, w))
+    }
+  }
+
+  def runFromPC(word: List[Char], from: List[BigInt]): Boolean = {
+    require(validStates(from))
+    validStates(runFrom(word, from))
+  }.holds because {
+    word match {
+      case Nil() => trivial
+      case Cons(w, ws) => movePC(from, w) && runFromPC(ws, move(from, w))
     }
   }
 
