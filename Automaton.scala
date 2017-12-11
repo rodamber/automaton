ff096430e27ec6573bd7b4ecd72165a4d167f9ee
rodamber <rodrigoambernardo@gmail.com>
2017-12-11 03:39:05 +0100

    Proof of concept is up. Now how to represent ε?

--- Automaton.scala
+++ Automaton.scala
@@ -1,3 +1,5 @@
+// In the current formulation, the automaton might not be finite.
+
 package automaton
 
 import stainless._
@@ -9,90 +11,43 @@ import stainless.proof._
 
 import set._
 
+object DFA {
+  def apply[State, Sym](nfa: NFA[State, Sym]): DFA[Set[State], Sym] = {
+    DFA({ (s: Set[State], w: Sym) => nfa.epsClosure(nfa.move(s, w)) })
+  }
+}
 
-// In the current formulation, the automaton might not be finite, so there is
-// no guarantees that it might ever terminate.
-case class Automaton[State, Sym](
-  trans: (State, Sym) => Set[State],
-  eps: Sym
-) {
+case class DFA[State, Sym](trans: (State, Sym) => State) {
+  def move(state: State, w: Sym): State = trans(state, w)
 
-  def move(states: Set[State], w: Sym): Set[State]  = {
+  def run(state: State, word: List[Sym]): State = {
+    word match {
+      case Nil() => state
+      case (w :: ws) => run(move(state, w), ws)
+    }
+  }
+}
+
+case class NFA[State, Sym](trans: (State, Sym) => Set[State]) {
+  def move(states: Set[State], w: Sym): Set[State] = {
     states match {
       case (ss + s) => trans(s, w) ++ move(ss, w)
       case _ => Set.empty
     }
   }
 
-  def epsClosed(states: Set[State]): Boolean = {
-    states == epsClosure(states)
-  }
-
-  def epsClosure(states: Set[State]): Set[State] = {
-    val newStates = states flatMap { trans(_, eps) }
-
-    if (newStates == states) newStates
-    else epsClosure(newStates)
-  }
-
   def run(states: Set[State], word: List[Sym]): Set[State] = {
     word match {
       case Nil() => states
-      case (w :: ws) => run(move(states, w), ws)
+      case (w :: ws) => run(move(states, w), ws) // We should apply ε-closure here.
     }
   }
 
-  def det: Automaton[Set[State], Sym] = {
-    Automaton({ (s: Set[State], w: Sym) => Set.set(epsClosure(move(s, w))) }, eps)
+  def epsClosure(states: Set[State]): Set[State] = {
+    ???
   }
-
-  def detIsDeterministic(s: Set[State], w: Sym): Boolean = {
-    det.trans(s, w).size == 1
-  }.holds because {
-    det.trans(s, w).size                             ==| trivial |
-    Set.set(move(s, w)).size                         ==| trivial |
-    Set.set(List(move(s, w))).size                   ==| trivial |
-    Set(List(move(s, w)).unique).size                ==| trivial |
-    List(move(s, w)).unique.size                     ==| trivial |
-    Cons(move(s, w), Nil().unique - move(s, w)).size ==| trivial |
-    Cons(move(s, w), Nil()).size                     ==| trivial |
-    BigInt(1)
-  }.qed
 
   def detSound(states: Set[State], word: List[Sym]): Boolean = {
-    require(epsClosed(states))
-    Set.set(run(states, word)) == det.run(Set.set(states), word - eps)
-  }.holds because {
-    (word, word - eps) match {
-      case (_, Nil()) => { // word is composed only of ε's
-        det.run(Set.set(states), Nil()) ==| trivial |
-        Set.set(states)                 ==| someLemma1 && someLemma2 | // saying that word is only ε's and that that makes run output states
-        Set.set(run(states, word))
-      }.qed
-
-      case ((w :: ws), (z :: zs)) => {
-        det.run(Set.set(states), word - eps)               ==| trivial |
-        det.run(Set(List(states).unique), word - eps)      ==| Set.isSet(List(states)) |
-        det.run(Set(List(states)), word - eps)             ==| trivial |
-        det.run(det.move(Set(List(states)), z), zs)        ==| trivial |
-        det.run(det.trans(states, z) ++ Set.empty, zs)     ==| ListSpecs.rightUnitAppend(det.trans(states, w).list) |
-        det.run(det.trans(states, z), zs)                  ==| trivial |
-        det.run(Set.set(epsClosure(move(states, z))), zs)  ==| detSound(epsClosure(move(states, z)), zs) |
-        Set.set(run(epsClosure(move(states, w)), ws))
-      }.qed && (
-        if (w == eps) {
-          Set.set(run(epsClosure(move(states, w)), ws))    ==| epsClosed(states) |
-          Set.set(run(states, ws))                         ==| (epsClosed(states) && (w == eps)) |
-          Set.set(run(states, word))
-        }.qed else {
-          Set.set(run(epsClosure(move(states, w)), ws))    ==| epsClosed(states) |
-            // ...
-          Set.set(run(states, word))
-        }.qed
-      )
-
-      case _ => trivial
-    }
-  }
-
+    run(states, word) == det.run(Set.set(states), word)
+  }.holds
 }
