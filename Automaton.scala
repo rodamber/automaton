6e1d525fb527c6980695f7eb8b2c648268663ce3
rodamber <rodrigoambernardo@gmail.com>
2017-12-11 04:24:55 +0100

    Proof with ε-moves :)

--- Automaton.scala
+++ Automaton.scala
@@ -12,12 +12,43 @@ import stainless.proof._
 import set._
 
 object DFA {
+
   def apply[State, Sym](nfa: NFA[State, Sym]): DFA[Set[State], Sym] = {
-    DFA({ (s: Set[State], w: Sym) => nfa.epsClosure(nfa.move(s, w)) })
+    DFA({ (s: Set[State], w: Sym) => nfa.epsClosure(nfa.move(s, Some(w))) })
   }
+
+  def dfaEquivNfa[State, Sym](nfa: NFA[State, Sym], states: Set[State],
+                              word: List[Sym]): Boolean = {
+    require(nfa.epsClosed(states))
+    val dfa = DFA(nfa)
+
+    nfa.run(states, word) == dfa.run(states, word)
+  }.holds because {
+    val dfa = DFA(nfa)
+
+    word match {
+      case Nil() => {
+        nfa.run(states, word)  ==| trivial               |
+        nfa.epsClosure(states) ==| nfa.epsClosed(states) |
+        states                 ==| trivial               |
+        dfa.run(states, word)
+      }.qed
+
+      case (w :: ws) => {
+        dfa.run(states, word)                                  ==| trivial |
+        dfa.run(dfa.move(states, w), ws)                       ==| trivial |
+        dfa.run(dfa.trans(states, w), ws)                      ==| trivial |
+        dfa.run(nfa.epsClosure(nfa.move(states, Some(w))), ws) ==| dfaEquivNfa(nfa, nfa.epsClosure(nfa.move(states, Some(w))), ws) |
+        nfa.run(nfa.epsClosure(nfa.move(states, Some(w))), ws) ==| trivial |
+        nfa.run(states, word)
+      }.qed
+    }
+  }
+
 }
 
 case class DFA[State, Sym](trans: (State, Sym) => State) {
+
   def move(state: State, w: Sym): State = trans(state, w)
 
   def run(state: State, word: List[Sym]): State = {
@@ -26,10 +57,14 @@ case class DFA[State, Sym](trans: (State, Sym) => State) {
       case (w :: ws) => run(move(state, w), ws)
     }
   }
+
 }
 
-case class NFA[State, Sym](trans: (State, Sym) => Set[State]) {
-  def move(states: Set[State], w: Sym): Set[State] = {
+// We represent ε-moves as by passing a None() to the transition function.
+// The ε character/word is never representend explicitly.
+
+case class NFA[State, Sym](trans: (State, Option[Sym]) => Set[State]) {
+  def move(states: Set[State], w: Option[Sym]): Set[State] = {
     states match {
       case (ss + s) => trans(s, w) ++ move(ss, w)
       case _ => Set.empty
@@ -38,16 +73,22 @@ case class NFA[State, Sym](trans: (State, Sym) => Set[State]) {
 
   def run(states: Set[State], word: List[Sym]): Set[State] = {
     word match {
-      case Nil() => states
-      case (w :: ws) => run(move(states, w), ws) // We should apply ε-closure here.
+      case Nil() => epsClosure(states)
+      case (w :: ws) =>
+        val newStates = move(states, Some(w))
+        run(epsClosure(newStates), ws)
     }
   }
 
   def epsClosure(states: Set[State]): Set[State] = {
-    ???
+    val newStates = move(states, None())
+
+    if (newStates == states) newStates
+    else epsClosure(newStates)
+  } ensuring { epsClosed _ }
+
+  def epsClosed(states: Set[State]): Boolean = {
+    states == epsClosure(states)
   }
 
-  def detSound(states: Set[State], word: List[Sym]): Boolean = {
-    run(states, word) == det.run(Set.set(states), word)
-  }.holds
 }
